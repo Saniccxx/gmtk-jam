38a0c8952cf38d71d885b9ce69198234773c393a
rutra8002 <96191787+rutra8002@users.noreply.github.com>
2026-07-13 11:36:33 +0200

    added jeff the jeff, jeff the physics, jeff the camera and jeff the input map
    ur welcome

--- player.gd
+++ player.gd
@@ -1,25 +1,44 @@
 extends CharacterBody2D
-
-
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
+const GROUND_ACCEL = 10.0
+const GROUND_FRICTION = 10.0
+const AIR_ACCEL = 0.5
+const AIR_FRICTION = 0.01
+const SLIDE_GRAVITY = 500.0
 
-
+	
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	var sliding := Input.is_action_pressed("slide")
+
+	if Input.is_action_pressed("jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+	
+	# walljump
+	if Input.is_action_pressed("jump") and is_on_wall() and not is_on_floor():
+		var wall_normal = get_wall_normal()
+		velocity.x = wall_normal.x * SPEED
 		velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
+	var direction := Input.get_axis("move_left", "move_right")
+	if sliding:
+		direction = 0
+
+	if is_on_floor():
+		if sliding:
+			#wierd thingies which calculate vector of player sliding down
+			var floor_normal = get_floor_normal()
+			var downhill = -Vector2(floor_normal.y, -floor_normal.x)
+
+			velocity += downhill * SLIDE_GRAVITY * delta
+		else:
+			velocity.x -= GROUND_FRICTION * velocity.x * delta
+			velocity.x += GROUND_ACCEL * direction * SPEED * delta
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.x -= AIR_FRICTION * velocity.x * delta
+		velocity.x += AIR_ACCEL * direction * SPEED * delta
 
 	move_and_slide()
