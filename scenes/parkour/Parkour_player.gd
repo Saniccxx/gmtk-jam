b2d160904429267ed5cf57a12a4c8c2eb73dcb35
rutra8002 <96191787+rutra8002@users.noreply.github.com>
2026-07-21 17:50:03 +0200

    changed values
    a bit

--- scenes/parkour/Parkour_player.gd
+++ scenes/parkour/Parkour_player.gd
@@ -1,11 +1,11 @@
 extends CharacterBody2D
 const SPEED = 1000.0
-const JUMP_VELOCITY = -3500.0
+const JUMP_VELOCITY = -5000.0
 const GROUND_ACCEL = 20.0
 const GROUND_FRICTION = 10.0
 const AIR_ACCEL = 5
 const AIR_FRICTION = 0.5
-const MASS = 3
+const MASS = 10
 
 @onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
 	
