2a6d1cbe09bf8be7c0fb49c8e086bb7a6f562a67
Tobiasz Małyszek <malyszektobiasz@gmail.com>
2026-07-21 17:28:27 +0200

    you can actually buy guns
    shop

--- global.gd
+++ global.gd
@@ -1,7 +1,7 @@
 extends Node
 
 var map_pos = Vector2(0,0)
-var money = 0
+var money = 9999
 var gun_names = ["pistol", "uzi", "shotgun", "machinegun"]
 var gun_costs = [0, 100, 2000, 100000]
 var gun_imgs = [0, "res://assets/machinegun.jpg", "res://assets/shotgun.jpg", "res://assets/uzi.png"]
--- scenes/shop/shop_item.gd
+++ scenes/shop/shop_item.gd
@@ -3,16 +3,31 @@ extends Control
 @export var item_num: String
 @export var item_icon: Texture2D
 
+
 @onready var label = $VBoxContainer/Label
 @onready var button = $VBoxContainer/Button
+@onready var cost = global.gun_costs[int(item_num)]
 
 func _ready():
-	label.text = str(global.gun_costs[int(item_num)])
+	label.text = str(cost)
 	button.icon = item_icon
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if global.best_owned_gun >= int(item_num):
-		Label.text = "Owned"
+		label.text = "Owned"
 	pass
 	
+
+
+func _on_button_pressed() -> void:
+	if label.text == "Owned":
+		return
+	if global.money < cost:
+		print("can't afford")
+		return
+	if int(item_num) > global.best_owned_gun + 1:
+		return
+	global.money -= cost
+	global.best_owned_gun += 1
+	global.current_gun = global.best_owned_gun
