dc6ec5f989dc78446e2b30f9b597e0cf385459d7
rutra8002 <96191787+rutra8002@users.noreply.github.com>
2026-07-22 22:56:59 +0200

    credits display
    really simple and bad rn

--- scenes/death/death_display.gd
+++ scenes/death/death_display.gd
@@ -5,4 +5,4 @@ func _on_retry_pressed() -> void:
 		get_tree().change_scene_to_file(global.last_minigame_path)
 
 func _on_map_display_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/map/map_display.tscn")
+	get_tree().change_scene_to_file("res://scenes/credits/credit_display.tscn")
