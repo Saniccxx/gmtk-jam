0cc09294dd44e9b1514efabdeae5a3bc49aae836
Sanuuu <shevchenko_oleksandr@outlook.com>
2026-07-23 18:02:45 +0200

    difficulty selection before zombie game and fixes
    map menu button now works in zombie game

--- scenes/difficulty_selection/difficulty_selection.gd
+++ scenes/difficulty_selection/difficulty_selection.gd
@@ -1,1 +1,16 @@
 extends Control
+
+func _on_easy_pressed() -> void:
+	global.current_difficulty = global.Difficulty.EASY
+	start_game()
+
+func _on_medium_pressed() -> void:
+	global.current_difficulty = global.Difficulty.MEDIUM
+	start_game()
+
+func _on_hard_pressed() -> void:
+	global.current_difficulty = global.Difficulty.HARD
+	start_game()
+
+func start_game() -> void:
+	get_tree().change_scene_to_file("res://scenes/zombie/zombie_game.tscn")
