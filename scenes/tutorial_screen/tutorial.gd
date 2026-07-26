a3c92b073933831c09b46b0d0c5e1dd81842f91f
Tobiasz Małyszek <malyszektobiasz@gmail.com>
2026-07-26 16:51:56 +0200

    tutorial basic

--- scenes/tutorial_screen/tutorial.gd
+++ scenes/tutorial_screen/tutorial.gd
@@ -1,15 +1,19 @@
-extends Control
-
+extends CanvasLayer
+@export var mode: PackedScene
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass
+	pass # Replace with function body.
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
 
 
-func _on_button_pressed() -> void:
+func _on_map_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/map/map_display.tscn")
-	get_tree().change_scene_to_file("res://scenes/map/map_display.tscn")
+
+
+func _on_play_pressed() -> void:
+	pass # Replace with function body.
