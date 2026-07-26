5b78ce3961cb88f9b6d25d93c4ab0affbbf5b70a
Tobiasz Małyszek <malyszektobiasz@gmail.com>
2026-07-26 13:41:48 +0200

    added coins as lives in target

--- scenes/target/target_display.gd
+++ scenes/target/target_display.gd
@@ -23,10 +23,10 @@ func place_hitmark(pos):
 
 func _remove_heart():
 	if lives_left == 3:
-		$Hud/CanvasLayer2/Hearts/TextureRect3/Heart3.play("Broken")
+		$Hud/CanvasLayer2/Hearts/TextureRect3/Heart3.visible = false
 	elif lives_left == 2:
-		$Hud/CanvasLayer2/Hearts/TextureRect2/Heart2.play("Broken")
+		$Hud/CanvasLayer2/Hearts/TextureRect2/Heart2.visible = false
 	if lives_left == 1:
-		$Hud/CanvasLayer2/Hearts/TextureRect/Heart1.play("Broken")
+		$Hud/CanvasLayer2/Hearts/TextureRect/Heart1.visible = false
 	lives_left -= 1
 	
