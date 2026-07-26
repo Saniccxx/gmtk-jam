80eaafc5f2aec3343619303da3643f5ae4a88cc6
Sanuuu <shevchenko_oleksandr@outlook.com>
2026-07-26 16:56:51 +0200

    hands are now rotating

--- scenes/zombie/zombie_player.gd
+++ scenes/zombie/zombie_player.gd
@@ -5,7 +5,17 @@ extends CharacterBody2D
 @export var ShootingSound: AudioStreamPlayer
 @export var ShotgunSound: AudioStreamPlayer
 @export var ReloadSound: AudioStreamPlayer
+
+
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
+@onready var shoulders: Node2D = $Shoulders
+@onready var hands_with_gun: Sprite2D = $Shoulders/HandsWithGun
+
+
+@export var pistol_texture: Texture2D
+@export var uzi_texture: Texture2D
+@export var shotgun_texture: Texture2D
+@export var machinegun_texture: Texture2D
 
 var can_take_damage: bool = true
 @export var invulnerability_time: float = 0.7
@@ -31,6 +41,15 @@ func _on_gun_changed():
 	is_reloading = false
 	ReloadSound.stop()
 	update_ammo_label()
+	match global.current_gun:
+		0:
+			hands_with_gun.texture = pistol_texture
+		1:
+			hands_with_gun.texture = uzi_texture
+		2:
+			hands_with_gun.texture = shotgun_texture
+		3:
+			hands_with_gun.texture = machinegun_texture
 
 func update_ammo_label():
 	update_labels.emit()
@@ -40,9 +59,11 @@ func play_animation(anim_name: String, flipped_h: bool = false) -> void:
 		"idle":
 			animated_sprite.position = Vector2(-6, -53)
 			animated_sprite.scale = Vector2(0.725, 0.725)
+			shoulders.position = Vector2(-9, -39)
 		"run":
 			animated_sprite.position = Vector2(-40 if not flipped_h else 20, -98)
 			animated_sprite.scale = Vector2(1.035, 1.035)
+			shoulders.position = Vector2(-3 if not flipped_h else -15, -26)
 	
 	animated_sprite.play(anim_name)
 
@@ -75,6 +96,7 @@ func _physics_process(_delta: float) -> void:
 
 
 func _process(_delta: float) -> void:
+	rotate_hands()
 	var current_weapon = global.weapons[global.current_gun]
 	if current_weapon.is_auto and can_shoot and Input.is_action_pressed("click"):
 		shoot()
@@ -123,6 +145,14 @@ func spawn_bullet() -> void:
 	bullet.rotation = final_rotation
 	
 	get_parent().add_child(bullet)
+
+func rotate_hands() -> void:
+	shoulders.look_at(get_global_mouse_position())
+	var mouse_pos: Vector2 = get_global_mouse_position()
+	if mouse_pos.x < global_position.x:
+		hands_with_gun.flip_v = true
+	else:
+		hands_with_gun.flip_v = false
 
 func reload() -> void:
 	if is_reloading:
